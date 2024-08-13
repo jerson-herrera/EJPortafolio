6c5e075a759e9bd127b070fbb75f0efa171a478e
Jerson Herrera <jeysonhwr@gmail.com>
2024-08-13 15:44:11 -0500

    Cambios en el css

---
+++ src/Components/Datos/AboutFrameworkdData.jsx
@@ -0,0 +1,16 @@
+export const frameworksLibrerias = [
+  {
+    techIcons: [
+      {
+        src: "./node-js.svg",
+        alt: "node-js-logo",
+        className: "nodelogo",
+      },
+      {
+        src: "./react.svg",
+        alt: "react-logo",
+        className: "reactlogo",
+      },
+    ],
+  },
+];
